61b482cc7862a40b57246801cd308250ed44ec44
Ubuntu <ubuntu@penguin.lxd>
2025-10-19 17:31:44 -0400

    first commit

---
+++ netlify/functions/updateAgentsContacted.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+var murl = process.env.MONGODB_URI;
+const client = new MongoClient(murl);
+client.connect();
+const database = client.db("mydata");
+let collection = database.collection("AgentsContacted");
+let firstNum = 0;
+let lastNum = 3;
+let myArgs = process.argv.slice(2);
+console.log(myArgs);
+
+async function upsertToBucket(coll, objArr) {
+  for (let i = 0; i < objArr.length; i++) {
+    const obj = objArr[i];
+    // Use listing_id as the unique identifier in the filter.
+    const filter = { listing_id: obj.listing_id };
+    try {
+      const result = await coll.updateOne(
+        filter,
+        { $set: obj },
+        { upsert: true }
+      );
+      if (result.upsertedCount > 0) {
+        console.log(
+          `Upsert created a new listing with id: ${result.upsertedId._id}`
+        );
+      } else if (result.modifiedCount > 0) {
+        console.log(`Updated listing with listing_id: ${obj.listing_id}`);
+      } else {
+        console.log(`No changes made for listing_id: ${obj.listing_id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+exports.handler = async function (event, context) {
+  const myObjArray = JSON.parse(event.body);
+  console.log(myObjArray);
+
+  await upsertToBucket(collection, myObjArray);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: myObjArray,
+    }),
+  };
+};

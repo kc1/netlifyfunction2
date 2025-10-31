060a17f69f62aa4e21664bfa3192aa08998dc891
Ubuntu <ubuntu@penguin.lxd>
2025-10-31 14:50:51 -0400

    first commit

--- netlify/functions/openRouter.js
+++ netlify/functions/openRouter.js
@@ -76,22 +76,21 @@ exports.handler = async (event, context) => {
   let waterFile, contourFile, roadFile;
   for (let i = 0; i < objArr.length; i++) {
     const obj = objArr[i];
-    if (obj.WaterURL && obj.WaterResponse.includes(["","{}"])) {
+    if (obj.WaterURL && ['', '{}'].includes(obj.RoadResponse)) {
       waterFile = obj.WaterURL;
       console.log("Water File: " + waterFile);
       promises.push(openRouterApiRequest(waterFile, waterText));
       obj.WaterResponse = "PENDING";
       myObjs.push(obj);
     }
-    if (obj.ContourURL && obj.ContourResponse.includes(["","{}"])) {
-      
+    if (obj.ContourURL && ['', '{}'].includes(obj.ContourResponse)) {
       contourFile = obj.ContourURL;
       console.log("Contour File: " + contourFile);
       promises.push(openRouterApiRequest(contourFile, contourText));
       obj.ContourResponse = "PENDING";
       myObjs.push(obj);
     }
-    if (obj.ContourURL && obj.RoadResponse.includes(["","{}"])) {
+    if (obj.ContourURL && ['', '{}'].includes(obj.RoadResponse)) {
       // Note: using ContourURL for Road as well, adjust if needed
       roadFile = obj.ContourURL;
       console.log("Road File: " + roadFile);

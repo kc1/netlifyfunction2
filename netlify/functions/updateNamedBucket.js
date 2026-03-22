baf571b06df4d5bca91caa6b10c2849f88eb8663
Ubuntu <ubuntu@penguin.tail9cfcb7.ts.net>
2026-03-22 07:47:11 -0400

    first commit

---
+++ netlify/functions/updateNamedBucket.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+var murl = process.env.MONGODB_URI;
+const client = new MongoClient(murl);
+client.connect();
+const database = client.db("mydata");
+let firstNum = 0;
+let lastNum = 3;
+let myArgs = process.argv.slice(2);
+console.log(myArgs);
+
+async function upsertToBucket(coll, objArr) {
+  for (let i = 0; i < objArr.length; i++) {
+    const obj = objArr[i];
+    // Use ID as the unique identifier in the filter.
+    const filter = { ID : obj.ID };
+    try {
+      const result = await coll.updateOne(
+        filter,
+        { $set: obj },
+        { upsert: true },
+      );
+      if (result.upsertedCount > 0) {
+        console.log(
+          `Upsert created a new listing with id: ${result.upsertedId.ID}`,
+        );
+      } else if (result.modifiedCount > 0) {
+        console.log(`Updated listing with ID: ${obj.ID}`);
+      } else {
+        console.log(`No changes made for ID: ${obj.ID}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+/* exports.handler = async function (event, context) {
+  const myObjArray = JSON.parse(event.body);
+  console.log(myObjArray);
+
+  await upsertToBucket(collection, myObjArray);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: myObjArray,
+    }),
+  };
+} */
+
+  async function createUniqueIndexForListingId(coll) {
+  try {
+    const indexName = await coll.createIndex(
+      { ID : 1 },
+      { unique: true }
+    );
+    console.log(`Unique index created with name: ${indexName}`);
+    return indexName;
+  } catch (error) {
+    console.error("Error creating unique index for listing_id:", error);
+    throw error;
+  }
+}
+
+exports.handler = async function (event, context) {
+  // console.log("context: ", context);
+  // console.log("event: ", event);
+
+  // Parse the request body as an object
+  const requestData = JSON.parse(event.body);
+  const myObjArray = requestData.data; // The array of objects
+  const collectionName = requestData.collectionName; // The collection name
+  let collection = database.collection(collectionName);
+
+  console.log("Received data:", myObjArray);
+  console.log("Received collection name:", collectionName);
+
+  // Call the function to ensure a unique index on "listing_id" is created
+  await createUniqueIndexForListingId(collection);
+
+  await upsertToBucket(collection, myObjArray);
+
+  return {
+    statusCode: 204,
+    body: JSON.stringify({
+      message: "Data received successfully",
+    }),
+  };
+};
